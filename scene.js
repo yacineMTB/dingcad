c70e355331cac2fd3698f8094cd20940f15475f3
Yacine Brahimi <yacine.bram@gmail.com>
2025-11-04 12:10:53 -0500

    scene: make closer together

--- scene.js
+++ scene.js
@@ -11,6 +11,18 @@ try {
   xiaoBoard = buildXiaoEsp32C3();
 }
 
-export const scene = union(pcbAssembly, translate(xiaoBoard, [10, 0, 0]));
+const pcbBounds = boundingBox(pcbAssembly);
+const xiaoBounds = boundingBox(xiaoBoard);
+const gap = 0;
+
+const shiftX = pcbBounds.max[0] + gap - xiaoBounds.min[0];
+const pcbCenterY = (pcbBounds.min[1] + pcbBounds.max[1]) / 2;
+const xiaoCenterY = (xiaoBounds.min[1] + xiaoBounds.max[1]) / 2;
+const shiftY = pcbCenterY - xiaoCenterY;
+const shiftZ = pcbBounds.min[2] - xiaoBounds.min[2];
+
+const xiaoAligned = translate(xiaoBoard, [shiftX, shiftY, shiftZ]);
+
+export const scene = union(pcbAssembly, xiaoAligned);
 export const dtsPcb = pcbAssembly;
-export const xiaoEsp32C3 = xiaoBoard;
+export const xiaoEsp32C3 = xiaoAligned;
